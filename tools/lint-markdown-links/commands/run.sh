b8999f3c441328639d62d8660702c6ff82ea9586
Karl R. Wurst <karl@w-sts.com>
2022-07-01 19:28:51 +0000

    ci: make markdown-link-check display only links with errors

--- tools/lint-markdown-links/commands/run.sh
+++ tools/lint-markdown-links/commands/run.sh
@@ -14,7 +14,7 @@ get-markdown-files() {
 check-links-in-each-markdown-file() {
     # Call markdown-link-check (provided by Docker image) once (-n1) for each file passed on stdin
     # separated by null character (-0)
-    xargs -0 -n1 docker run --rm -v "${PWD}:/w" -w /w ghcr.io/tcort/markdown-link-check:stable
+    xargs -0 -n1 docker run --rm -v "${PWD}:/w" -w /w ghcr.io/tcort/markdown-link-check:stable -q
 }
 
 main
